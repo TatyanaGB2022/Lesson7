c53937bcda9ea0ffc00993a934494f62dd614790
Tatyana <tatyana.kurii@mail.ru>
2023-02-04 18:12:35 +0600

    Mision1

---
+++ Mission2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateMatrix()
+{
+    int[,] matrix = new int[3, 4];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = CreateMatrix();
+PrintArray(matrix);
---
+++ Mission3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых обе позиции чётные,
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int[,] GenereteArray(int row, int col)
+{
+  int[,] array = new int[row, col];
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      array[i, j] = new Random().Next(0, 10);
+    }
+  }
+  return array;
+}
+void PrintArray(int[,] array)
+{
+  int row = array.GetLength(0);
+  int col = array.GetLength(1);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      System.Console.Write($"{array[i, j]}\t");
+    }
+    System.Console.WriteLine();
+  }
+}
+
+void Power2EvenInd(int[,] array)
+{
+  int row = array.GetLength(0);
+  int col = array.GetLength(1);
+  for (int i = 1; i < row; i+=2)  // через 1
+  {
+    for (int j = 1; j < col; j+=2)
+    {
+      array[i, j] *= array[i, j];
+    }
+  }
+}
+
+int [,] array = GenereteArray(3, 4);
+PrintArray(array);
+System.Console.WriteLine();
+Power2EvenInd(array);
+PrintArray(array);
